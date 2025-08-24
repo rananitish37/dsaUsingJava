2b4931d6a15b205aa6b9bcaf0f4803bc526bce5c
nrana <rananitish37@gmail.com>
2025-08-24 17:19:01 +0530

    RemoveDuplicatesSortedArray

---
+++ Arrays/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RemoveDuplicatesSortedArray {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        // code here
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            if(!map.containsKey(arr[i]) || map.get(arr[i]) == 0){
+                map.put(arr[i], 1);
+            }else{
+                map.put(arr[i],1);
+            }
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(Map.Entry<Integer, Integer> entries: map.entrySet()){
+            if(entries.getValue()==1){
+                ans.add(entries.getKey());
+            }
+        }
+        Collections.sort(ans);
+        return ans;
+    }
+}
