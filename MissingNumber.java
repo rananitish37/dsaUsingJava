7c957ce6e55de84f87ff978557fe5568e72dc591
nrana <rananitish37@gmail.com>
2026-02-17 23:02:20 +0530

    tryign two pinter patterns

--- MissingNumber.java
+++ MissingNumber.java
@@ -2,19 +2,26 @@ import java.util.HashSet;
 
 public class MissingNumber {
     public static void main(String[] args) {
-        int n=5;
-        int numbers[] = {2,3,1,5};
+        // int n=5;
+        int nums[] = {2,3,1,5};
 
-        HashSet<Integer> set = new HashSet<>();
+        // HashSet<Integer> set = new HashSet<>();
 
-        for(int i: numbers){
-            set.add(i);
+        // for(int i: numbers){
+        //     set.add(i);
+        // }
+        // for(int i=1; i<=n; i++){
+        //     if(!set.contains(i)){ 
+        //         System.out.println(i);
+        //         break;
+        //     }
+        // }
+
+        int xor=0;
+        for(int i=0; i<nums.length; i++){
+            xor = xor^nums[i]^i;
         }
-        for(int i=1; i<=n; i++){
-            if(!set.contains(i)){ 
-                System.out.println(i);
-                break;
-            }
-        }
+        xor = xor^nums.length;
+        System.out.println(xor);
     }
 }
