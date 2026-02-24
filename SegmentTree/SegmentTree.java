c33fb1c55bb57eccc56a4e647591e79578de87c1
nrana <rananitish37@gmail.com>
2026-02-24 23:30:51 +0530

    Understanding query in segment tree

--- SegmentTree/SegmentTree.java
+++ SegmentTree/SegmentTree.java
@@ -20,6 +20,24 @@ public class SegmentTree {
 
         
     }
+    public static int getSum(int idx, int si, int sj, int qi, int qj){
+
+        if(qj <= si || qi >= sj){ //non overlapping
+            return 0;
+        }else if(si >= qi && sj <= qj){//total overlapping
+            return tree[idx];
+        }else{ //partial overlapping
+            int mid = (si + sj)/2;
+            int left = getSum(idx*2+1, si, mid, qi, qj);
+            int right = getSum(idx*2+2, mid+1, sj, qi, qj);
+            return left+right;
+        }
+    }
+
+    public static int segmentQuery(int []arr, int qi, int qj){
+        return getSum(0, 0, arr.length-1, qi, qj);
+
+    }
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int n= arr.length;
@@ -28,8 +46,11 @@ public class SegmentTree {
 
         build(arr, 0, 0,n-1);
 
-        for (int i=0; i<tree.length; i++) {
-            System.out.print(tree[i]+" ");
-        }
+        // for (int i=0; i<tree.length; i++) {
+        //     System.out.print(tree[i]+" ");
+        // }
+
+        System.out.print(segmentQuery(arr, 2, 4));
+       
     }
 }
